d5527cd874b8122c285a15bec00a702424ddcef3
Carlos <carloaless1512@gmail.com>
2025-03-24 20:57:49 -0300

    configurações de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'], // Adicione essa linha para incluir arquivos .glb como assets
-
-})
+  assetsInclude: ["**/*.glb"], // Adicione essa linha para incluir arquivos .glb como assets
+  base: "/Prime",
+});
