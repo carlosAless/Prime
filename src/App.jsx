426839d5054d375c70784b8285e3edfd089fed04
Carlos <carloaless1512@gmail.com>
2025-03-26 12:20:11 -0300

    configuraçoes de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
-import { NotFound } from "./pages/NotFound/NotFound"; // Importe a página 404
+import { NotFound } from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <>
-      <div className="container">
-        <Home></Home>
-      </div>
-    </>
+    <div className="container">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </div>
   );
 }
 
