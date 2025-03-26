7a03fb1f8571a8bee9d246cd70f38e027dde9fb7
Carlos <carloaless1512@gmail.com>
2025-03-26 12:30:38 -0300

    configuraçoes de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Importe o BrowserRouter
+import { HashRouter, Router } from "react-router-dom"; // Importe o BrowserRouter
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>
+  </Router>
 );
