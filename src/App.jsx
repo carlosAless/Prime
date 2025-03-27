41f1c6f26c34c2ad2a0da41ad794d76fa5f6cf29
Carlos <carloaless1512@gmail.com>
2025-03-26 21:06:49 -0300

    configuraçoes de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/a" element={<NotFound />} />
         </Routes>
-        <Home></Home>
       </div>
     </>
   );
